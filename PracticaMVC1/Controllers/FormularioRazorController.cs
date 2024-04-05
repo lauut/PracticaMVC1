5766c906c58eaa419df733a51e5d143def0b7fae
lauut <lauutjd23@gmail.com>
2024-04-05 16:42:39 -0600

    guia 8 :)

---
+++ PracticaMVC1/Controllers/FormularioRazorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaMVC1.Controllers
+{
+    public class FormularioRazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
